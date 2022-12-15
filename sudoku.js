343d49c33972c30fed5498816b01f97ec2adaecf
Azzirr <arek.sliwowski37@gmail.com>
2022-12-15 15:41:09 +0100

    Created basics of sudoku, time to add solver algorithm

--- sudoku.js
+++ sudoku.js
@@ -7,5 +7,18 @@ const board = [ [7, n, 4, 8, n, n, 3, n, 1],
                 [9, n, 7, n, n, n, n, 3, 2],
                 [n, n, 6, n, 1, 5, 4, n, n],
                 [n, 7, n, n, n, 9, n, 6, 5],
-                [5, n, 8, n, n, 2, 1, n, 3]]
+                [5, n, 8, n, n, 2, 1, n, 3]
+              ]
+// putting correct board on the screen
+function putBoard(){
+    let count = 0
+    for(let x = 0; x < board.length; x++){
+        for(let i = 0; i < board[x].length; i++){
+            let groundId = document.getElementById((count + 1));
+            groundId.innerHTML = board[x][i]
+            count++
+        }
+    }
+}
+putBoard()
 
