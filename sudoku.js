c65240c0bceebf59364a6481a76ab4405cdeddf4
Azzirr <arek.sliwowski37@gmail.com>
2022-12-16 16:05:36 +0100

    .

--- sudoku.js
+++ sudoku.js
@@ -9,6 +9,7 @@ const board = [ [7, n, 4, 8, n, n, 3, n, 1],
                 [n, 7, n, n, n, 9, n, 6, 5],
                 [5, n, 8, n, n, 2, 1, n, 3]
               ]
+const verticalBoard = [[], [], [], [], [], [], [], [], []];
 // putting correct board on the screen
 function putBoard(){
     let count = 0
@@ -20,5 +21,30 @@ function putBoard(){
         }
     }
 }
-putBoard()
 
+// create vertical arrays
+function verticalArrays(){
+    for(let j = 0; j < 9; j++){
+        for(let i = 0; i < board.length; i++){
+            verticalBoard[j].push(board[i].slice(0, 1))
+        }
+    }
+}
+verticalArrays()
+
+// pushing unique values to null items in horizontal array
+
+function pushHorizontal(){
+    function generateRandomNumber(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[i].length; j++){
+            if(board[i][j] === null){
+                board[i][j] = generateRandomNumber(1, 9)
+            }
+        }
+    }
+}
+pushHorizontal()
+putBoard()
