e39bf4a5eb3251979a5beb06a05a46c0f4ea2659
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-01-07 20:43:44 +0100

    Finally done sudoku exercise

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,5 @@
 const n = null;
-const board = [ [7, n, 4, 8, n, n, 3, n, 1],
+let board = [   [7, n, 4, 8, n, n, 3, n, 1],
                 [8, 2, n, 5, n, n, n, 4, n],
                 [n, n, 9, 4, 3, n, 5, n, n],
                 [3, 1, n, n, n, n, 8, n, 7],
@@ -9,42 +9,83 @@ const board = [ [7, n, 4, 8, n, n, 3, n, 1],
                 [n, 7, n, n, n, 9, n, 6, 5],
                 [5, n, 8, n, n, 2, 1, n, 3]
               ]
-const verticalBoard = [[], [], [], [], [], [], [], [], []];
+const boardA = [[n, 2, n, n, n, 9, 5, n, n],
+                [9, n, n, 7, n, n, 3, 2, n],
+                [6, n, n, 4, 5, n, 1, n, n],
+                [8, n, 1, 5, n, n, n, 6, 2],
+                [3, 4, n, 6, n, n, n, 1, n],
+                [2, 5, 6, 1, n, n, 4, 3, n],
+                [1, n, n, n, n, 4, n, n, 8],
+                [n, n, 4, 8, 3, 5, 2, n, n],
+                [5, n, 8, n, n, n, 7, 4, n]
+               ]
+const boardB = [[9, 1, n, n, n, n, n, 8, n],
+                [n, n, 7, 9, 6, n, 3, n, n],
+                [n, n, n, n, n, 5, n, n, n],
+                [n, n, 6, 4, 3, n, 9, n, n],
+                [n, n, n, n, 2, n, n, n, n],
+                [7, n, n, n, n, n, n, n, 6],
+                [n, n, 4, n, n, 2, n, n, n],
+                [n, n, n, n, n, 7, 5, n, n],
+                [8, n, n, 5, 4, n, n, 3, n]
+               ]
+
 // putting correct board on the screen
-function putBoard(){
+function putBoard(data){
     let count = 0
-    for(let x = 0; x < board.length; x++){
-        for(let i = 0; i < board[x].length; i++){
+    for(let x = 0; x < data.length; x++){
+        for(let i = 0; i < data[x].length; i++){
             let groundId = document.getElementById((count + 1));
-            groundId.innerHTML = board[x][i]
+            groundId.innerHTML = data[x][i]
             count++
         }
     }
 }
 
-// create vertical arrays
-function verticalArrays(){
-    for(let j = 0; j < 9; j++){
-        for(let i = 0; i < board.length; i++){
-            verticalBoard[j].push(board[i].slice(0, 1))
+function isValid(board, row, col, k){
+    for(let i = 0; i < 9; i++){
+        const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+        const n = 3 * Math.floor(col / 3) + i % 3;
+        if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+          return false;
         }
     }
+    return true;
 }
-verticalArrays()
 
-// pushing unique values to null items in horizontal array
-
-function pushHorizontal(){
-    function generateRandomNumber(min, max){
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    for(let i = 0; i < board.length; i++){
-        for(let j = 0; j < board[i].length; j++){
-            if(board[i][j] === null){
-                board[i][j] = generateRandomNumber(1, 9)
+function sudokuSolver(data) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (data[i][j] == null) {
+          for (let k = 1; k <= 9; k++) {
+            if (isValid(data, i, j, k)) {
+              data[i][j] = k;
+            if (sudokuSolver(data)) {
+             return true;
+            } else {
+             data[i][j] = null;
             }
-        }
-    }
+           }
+         }
+         return false;
+       }
+     }
+   }
+   return true;
+  }
+
+//Board 1 - from task
+function board1(){
+    sudokuSolver(board);
+    putBoard(board)
 }
-pushHorizontal()
-putBoard()
+//Board 2 - easy
+function board2(){
+    sudokuSolver(boardA);
+    putBoard(boardA)
+}
+//Board 3 - hard
+function board3(){
+    sudokuSolver(boardB);
+    putBoard(boardB)
+}
